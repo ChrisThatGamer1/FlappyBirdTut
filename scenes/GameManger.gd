f2b2aee95d54bcb62565754e826ed266601bd423
Chris <clarkctg300@gmail.com>
2024-06-29 12:31:04 -0400

    5th commit - Game Manager
    Added code for the game manager, but having some sort of bugs. Going to try and debug.

--- scenes/GameManger.gd
+++ scenes/GameManger.gd
@@ -1,10 +1,25 @@
 extends Node
 
-@onready var player_bird = $"../PlayerBird"
+@onready var player_bird = $"../PlayerBird" as PlayerBird
 @onready var pipe_spawner = $"../PipeSpawner"
-@onready var ground = $"../Ground"
+@onready var ground = $"../Ground" as Ground
 
 var points = 0 
  
 func _ready():
-	pass
+	player_bird.game_started.connect(on_game_started)
+	ground.bird_crashed.connect(end_game)
+	pipe_spawner.bird_crashed.connect(end_game)
+	pipe_spawner.point_scored.connect(on_point_scored)
+
+func on_game_started():
+	pipe_spawner.start_spawning_pipes()
+
+
+func end_game(): #function to stop the game after the player crashes
+	ground.stop()
+	player_bird.stop()
+	pipe_spawner.stop()
+
+func on_point_scored():
+	points += 1
